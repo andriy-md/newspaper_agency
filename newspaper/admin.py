c6de35360b8fc3b60499a7827354552549bceb77
Andriy <an_mydel@gmail.com>
2023-06-05 11:39:24 +0300

    Register all models to admin.py

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from newspaper.models import Redactor, Topic, Newspaper
+
+
+@admin.register(Redactor)
+class RedactorAdmin(admin.ModelAdmin):
+    list_display = admin.ModelAdmin.list_display + (
+        "username", "first_name", "last_name", "email", "years_of_experience",
+    )
+    fieldsets = [
+        (
+            "Main Information",
+            {
+                "fields": ["username", "first_name", "last_name", "email", "years_of_experience"]
+            }
+        ),
+        (
+            "Advanced",
+            {
+                "fields": [
+                    "is_staff",
+                    "is_superuser",
+                    "is_active",
+                    "last_login",
+                    "date_joined",
+                    "groups",
+                    "user_permissions"
+                ]
+            }
+        )
+    ]
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ["title", "topic"]
