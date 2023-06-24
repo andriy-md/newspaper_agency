63ae54bed456cd1475577756032c81304ad8a82c
Andriy <an_mydel@gmail.com>
2023-06-24 23:40:54 +0300

    Link to PostgreSQAL database

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -89,6 +90,12 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600,
+    conn_health_checks=True,
+)
 
 
 # Password validation
